4c0a96c251a5fbd398bfc870b8bcda05bea132c2
jaldhara.patel <jaldharapatel91@gmail.com>
2023-04-28 11:38:18 -0400

    Added password criteria to javascript file

---
+++ script.js
@@ -0,0 +1,36 @@
+
+var generateBtn = document.querySelector("#generate");
+
+// Adding password criteria including password length, adding arrays for lowercase,uppercase,numeric,and special chaeacters
+var passwordcriteria = {
+  passwordLenght: 6,
+
+  passwordLowerCase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+    "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+
+  passwordUpperCase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+    "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
+
+  passwordNumber: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+
+  passwordCharacter: ["!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",",
+    "-", ".", "/", "\\", ":", ";", "<", ">", "=", "?", "@", "[", "]", "^", "_", "`", "{", "}", "|", "~"]
+}
+
+function generatePassword(){
+  console.log('Hey! You click generatePassword button.');
+
+  return "Generated password will go here!"
+  }
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
